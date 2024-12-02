127df7a5ca09176666f3231f15c503961ab25e9f
Anna <annaprando04@gmail.com>
2024-12-01 22:42:14 -0300

    adiciona auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,24 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TasksModule } from './tasks/task.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './auth/auth.guard';
 
 @Module({
-  imports: [ TasksModule,
+  imports: [ 
+    TasksModule,
     UsersModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/nest'), 
+    MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+    AuthModule, 
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
